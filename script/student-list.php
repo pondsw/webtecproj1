c270c008fdef969d3993c006c10c8dd24377ca05
boomauakim <boomauakim@hotmail.com>
2017-03-27 02:13:00 +0700

    add student-list.php

---
+++ script/student-list.php
@@ -0,0 +1,38 @@
+<?php
+include ('database-connect.php');
+
+$conn = connect();
+$section = $_GET['section'];
+
+function showStudentList($stmtexec){
+     echo "<table style='border: solid 1px black;'>";
+     echo "<tr><th>Student ID</th><th>Student Name</th></tr>";
+
+     foreach($stmtexec->fetchAll() as $k) {
+          echo "<tr>";
+          echo "<td style='width: 150px; border: 1px solid black;'>".$k['studentid']."</td>";
+          echo "<td style='width: 150px; border: 1px solid black;'><a href=\"student-info.php?studentid=".$k['studentid']."\">".$k['fname']." ".$k['lname']."</a></td>";
+          echo "</tr>";
+     }
+
+     echo "</table>";
+}
+
+echo "<h3>Student List</h3>";
+
+try {
+     $stmt = $conn->prepare("SELECT studentid, fname, lname FROM takescourse As tc INNER JOIN user As u ON tc.studentid = u.userid where sectionid='$section'");
+     $stmt->execute();
+
+     if ($stmt->rowCount()>0){
+          showStudentList($stmt);
+     }
+     else{
+          echo "<h3>Student Not Found !</h3>";
+     }
+}
+catch(PDOException $e) {
+     echo "Error: " . $e->getMessage();
+}
+$conn = null;
+?>
